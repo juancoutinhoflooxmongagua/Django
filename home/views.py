eb44906dae7201dc530a63eb31ce1cdc21b3868e
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-27 13:51:13 -0300

    (Atomic Commit): feat: finalizing project and init | functions basics and testing of django

---
+++ home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def home(request):
+    print("home")
+
+    context = {"text": "Olá home"}
+
+    return render(
+        request,
+        "home/index.html",
+        context,
+    )
